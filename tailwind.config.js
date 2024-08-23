390ce29ab9c53c55e5be5cbd5a4f199f096d0ca6
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-08-23 22:43:50 +0900

    chore: tailwind daisyUIを導入

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{vue,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ['business']
+  }
+}
+
