a8beb167c0810f8884938e2acdc4ffa983c01af5
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-08-24 06:00:30 +0900

    chore: ルーティングを追加

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
+import Todo from "@page/Todo.vue";
 import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from "./App.vue";
 import "./assets/global.css";
 
-createApp(App).mount("#app");
+const routes = [
+  { path: '/', redirect: '/todo' },
+  { path: '/todo', name: 'todo', component: Todo },
+];
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
