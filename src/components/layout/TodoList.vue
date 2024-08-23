4277acb1c724846032fec0f0e1596de221775b43
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-08-24 05:05:15 +0900

    add: todo listを追加

---
+++ src/components/layout/TodoList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import TodoListItem from "@base/TodoListItem.vue";
+
+const props = defineProps<{
+  todoList: {
+    title: string;
+    lapTime: string;
+    checkable: boolean;
+    branchName?: string;
+  }[];
+}>();
+const emit = defineEmits<{
+  checkedTodo: [index: number];
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-1">
+    <TodoListItem
+      @checked-todo="emit('checkedTodo', index)"
+      v-for="(
+        { title, lapTime, checkable, branchName }, index
+      ) in props.todoList"
+      :title="title"
+      :lap-time="lapTime"
+      :branch-name="branchName"
+      :checkable="checkable"
+    />
+  </div>
+</template>
