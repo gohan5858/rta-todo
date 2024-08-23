ebefa5594f21addceb2fe989247ee574e4c4de05
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-08-24 02:04:16 +0900

    add: RTA式のタイマーを追加

---
+++ src/components/base/RTATimer.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { useIntervalFn } from "@vueuse/core";
+import { computed, onMounted, ref } from "vue";
+
+// ストップウォッチの経過時間（ミリ秒）を保持する変数
+const elapsedTime = ref(0);
+// ストップウォッチが動作中かどうかを管理する変数
+const isRunning = ref(false);
+
+// ストップウォッチを動かす関数
+const { pause, resume } = useIntervalFn(
+  () => {
+    elapsedTime.value += 10; // 10ミリ秒ごとに更新
+  },
+  10,
+  { immediate: false },
+);
+
+// ストップウォッチを開始する関数
+const start = () => {
+  if (!isRunning.value) {
+    resume();
+    isRunning.value = true;
+  }
+};
+// ストップウォッチを停止する関数
+const stop = () => {
+  if (isRunning.value) {
+    pause();
+    isRunning.value = false;
+  }
+};
+// ストップウォッチをリセットする関数
+const reset = () => {
+  pause();
+  elapsedTime.value = 0;
+  isRunning.value = false;
+};
+const lap = () => {
+  emits("lapButtonClick", elapsedTime.value);
+};
+
+// 経過時間をフォーマットする関数
+const formattedTime = computed((): string => {
+  return new Date(elapsedTime.value).toISOString().substring(11, 22);
+});
+
+const emits = defineEmits<{
+  lapButtonClick: [lappedElapsedTime: number];
+}>();
+onMounted(() => {
+  start();
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-between">
+    <div class="text-4xl">{{ formattedTime }}</div>
+    <div class="flex flex-row items-center justify-center gap-4">
+      <div class="flex flex-row gap-1">
+        <button class="btn btn-success btn-md w-14" @click="start">
+          Start
+        </button>
+        <button class="btn btn-secondary btn-md w-14" @click="stop">
+          Pause
+        </button>
+        <button class="btn btn-warning btn-md w-14" @click="reset">
+          Reset
+        </button>
+      </div>
+      <button class="btn btn-primary btn-md w-20" @click="lap">Lap</button>
+    </div>
+  </div>
+</template>
