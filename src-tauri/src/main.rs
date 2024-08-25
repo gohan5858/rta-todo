55be40a4bc5d7895e7bb465e7da75111aa820a70
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-08-26 08:13:01 +0900

    chore: invoke時に補完が効かなかったためspectaを導入

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use specta::collect_types;
+use tauri_specta::ts;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
+#[specta::specta]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
+    #[cfg(debug_assertions)]
+    ts::export(collect_types![greet], "../src/bindings.ts").unwrap();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
