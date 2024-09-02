788b79fd4e0ba3869ac0d28db9d2c5b0777d47ac
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-09-02 09:58:23 +0900

    change: 右ナビバーボタンをポップアップからドロワーに変更

--- src/components/page/Home.vue
+++ src/components/page/Home.vue
@@ -20,32 +20,37 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="grid grid-cols-1 grid-rows-[1fr_10fr_1fr] gap-2">
-    <HomeNavbar v-model="displayCompleted" />
-    <div class="flex flex-col gap-5 overflow-auto">
-      <div class="flex flex-col gap-2">
-        <RouterLink
-          class="flex"
-          v-for="todoList in todoLists"
-          :key="todoList.id"
-          :to="{
-            name: 'todo',
-            params: { id: todoList.id },
-          }"
-        >
-          <div class="btn flex-grow text-xl">
-            {{ todoList.title }}
+  <HomeNavbar v-model="displayCompleted">
+    <div class="flex flex-grow flex-col gap-5">
+      <!-- TODO list -->
+      <div class="relative flex-grow">
+        <div class="absolute flex h-full w-full flex-col overflow-auto">
+          <div class="flex flex-col gap-2">
+            <RouterLink
+              class="flex"
+              v-for="todoList in todoLists"
+              :key="todoList.id"
+              :to="{
+                name: 'todo',
+                params: { id: todoList.id },
+              }"
+            >
+              <div class="btn flex-grow text-xl">
+                {{ todoList.title }}
+              </div>
+            </RouterLink>
           </div>
-        </RouterLink>
+        </div>
+      </div>
+      <!-- Add new TODO button -->
+      <div class="flex flex-row items-center justify-center">
+        <button
+          v-if="!displayCompleted"
+          class="btn bg-orange-400 text-xl text-black hover:bg-orange-500"
+        >
+          +
+        </button>
       </div>
     </div>
-    <div class="flex flex-row items-center justify-center">
-      <button
-        v-if="!displayCompleted"
-        class="btn bg-orange-400 text-xl text-black hover:bg-orange-500"
-      >
-        +
-      </button>
-    </div>
-  </div>
+  </HomeNavbar>
 </template>
