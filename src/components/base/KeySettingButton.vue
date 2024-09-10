2de2052c28be3f8e9d1956d77c0db36de64ccd06
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-09-10 13:14:09 +0900

    add: キーバインド設定用のボタンを追加

---
+++ src/components/base/KeySettingButton.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { useMagicKeys } from "@vueuse/core";
+import { ref } from "vue";
+
+const currentKeys = ref<string[]>([]);
+const assignedKeys = ref<string[]>([]);
+const keyPopup = ref<HTMLDialogElement | null>(null);
+
+const { current } = useMagicKeys({
+  onEventFired(e: KeyboardEvent) {
+    // keyPopupが表示されていない場合はキー入力を受け付けない
+    if (!keyPopup.value?.open) return;
+
+    if (e.type === "keydown") {
+      currentKeys.value = Array.from(current).map((key) => {
+        if (key === "meta") {
+          return "⌘";
+        } else {
+          return key.toUpperCase();
+        }
+      });
+    } else {
+      assignedKeys.value = currentKeys.value;
+      keyPopup.value?.close();
+    }
+  },
+});
+</script>
+
+<template>
+  <div class="flex flex-row justify-between">
+    タイマー停止
+    <button
+      class="btn btn-neutral"
+      @click="
+        () => {
+          keyPopup?.showModal();
+        }
+      "
+    >
+      <kbd class="kbd" v-for="key in assignedKeys" :key="key">{{ key }}</kbd>
+    </button>
+    <dialog class="modal" ref="keyPopup">
+      <div class="modal-box">
+        <h3 class="text-center text-lg font-bold">任意の組み合わせを入力</h3>
+        <p class="flex flex-row justify-center gap-3 py-4">
+          <kbd class="kbd" v-for="key in currentKeys" :key="key">{{ key }}</kbd>
+        </p>
+      </div>
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+  </div>
+</template>
