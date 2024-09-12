ec6dd2a66a83a928fe0e02bf151dc95a0e310951
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-09-12 21:51:04 +0900

    change: 非同期のローディングに対応

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,24 @@ onMounted(async () => {
     :data-theme="theme"
     class="h-screen select-none overflow-hidden p-5 font-mono"
   >
-    <RouterView :key="route.fullPath" class="h-full w-full" />
+    <RouterView
+      :key="route.fullPath"
+      class="h-full w-full"
+      v-slot="{ Component }"
+    >
+      <Suspense timeout="0">
+        <!-- NOTE: divで囲むことでSuspense配下に複数のルートノードが来ることを防ぐ
+          (<Suspense> slots expect a single root node.対策) -->
+        <div class="h-full w-full">
+          <component :is="Component" />
+        </div>
+
+        <template #fallback>
+          <div class="flex h-full w-full items-center justify-center">
+            <span class="loading loading-dots loading-lg" />
+          </div>
+        </template>
+      </Suspense>
+    </RouterView>
   </div>
 </template>
