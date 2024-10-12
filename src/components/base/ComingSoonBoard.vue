ab75bb147b68df8f166315d3da55854f95b7333e
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-10-12 19:50:09 +0900

    add: ComingSoon機能に差し込むコンポーネントを追加

---
+++ src/components/base/ComingSoonBoard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div class="flex flex-col gap-1">
+      <div class="flex animate-pulse flex-col">
+        <h1 class="mb-4 text-center text-5xl font-bold">Coming Soon</h1>
+        <p class="text-xl">鋭意制作中です。もうしばらくお待ちください。</p>
+      </div>
+    </div>
+  </div>
+</template>
